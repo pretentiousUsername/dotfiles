4fbec80a358ebfe2fdf963a9531056c733d0f189
Ian Mitchell <mitchell.ian.2001@gmail.com>
2026-03-14 13:59:02 -0400

    edit rofi-powermenu launch script

--- rofi_apps/launch_power_menu.sh
+++ rofi_apps/launch_power_menu.sh
@@ -4,4 +4,4 @@ rofi \
   -show p \
   -modi p:'~/Projects/dotfiles/rofi_apps/rofi-power-menu/./rofi-power-menu --symbols-font "Symbols Nerd Font Mono"' \
   -font "JuliaMono NerdFont 10" \
-  -theme-str 'window {width: 8em;} listview {lines: 6;}'
+  -theme-str 'window {width: 10em;} listview {lines: 6;}'
