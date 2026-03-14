caf572b294fe22dd5fc329a42ec3afed70b91c3e
Ian Mitchell <mitchell.ian.2001@gmail.com>
2026-03-14 13:58:09 -0400

    edit rofi theme

--- rofi_apps/launch_power_menu.sh
+++ rofi_apps/launch_power_menu.sh
@@ -4,5 +4,4 @@ rofi \
   -show p \
   -modi p:'~/Projects/dotfiles/rofi_apps/rofi-power-menu/./rofi-power-menu --symbols-font "Symbols Nerd Font Mono"' \
   -font "JuliaMono NerdFont 10" \
-  -theme Paper \
   -theme-str 'window {width: 8em;} listview {lines: 6;}'
