8ee3ce628a147bd91d922766621313a014e05e62
Ian Mitchell <mitchell.ian.2001@gmail.com>
2026-06-10 20:38:08 -0400

    make rofi-power-menu work gud

--- rofi_apps/launch_power_menu.sh
+++ rofi_apps/launch_power_menu.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+  # -modi p:'~/Projects/dotfiles/rofi_apps/rofi-power-menu/./rofi-power-menu --symbols-font "Symbols Nerd Font Mono"' \
+  # -modi p:'~/Projects/dotfiles/rofi_apps/./rofi_power_menu --symbols-font "Symbols Nerd Font Mono"' \
 rofi \
   -show p \
-  # -modi p:'~/Projects/dotfiles/rofi_apps/rofi-power-menu/./rofi-power-menu --symbols-font "Symbols Nerd Font Mono"' \
   -modi p:'~/Projects/dotfiles/rofi_apps/./rofi_power_menu --symbols-font "Symbols Nerd Font Mono"' \
   -font "JuliaMono NerdFont 10" \
   -theme-str 'window {width: 10em;} listview {lines: 6;}'
